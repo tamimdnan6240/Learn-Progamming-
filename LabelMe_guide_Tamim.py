80b65c5bbfdd0000030202cbcde2b63f23a31ecb
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2025-05-09 20:52:09 -0400

    Create LabelMe_guide_Tamim.py

---
+++ LabelMe_guide_Tamim.py
@@ -0,0 +1,7 @@
+## Use LabelMe via PyQt5 in a Clean Virtual Environment 
+conda create -n labelme_env python=3.9 -y
+conda activate labelme_env
+
+pip install pyqt5==5.15.9
+pip install labelme==5.2.1
+labelme
