8167f27e1da898a6bafa31d1301a1d6346026ed1
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2025-05-09 21:22:56 -0400

    Update LabelMe-environment.py

--- LabelMe-environment.py
+++ LabelMe-environment.py
@@ -3,3 +3,11 @@ conda activate labelme_env
 
 pip install pyqt5==5.15.9
 pip install labelme==5.2.1
+
+## use the following lines for LabelMe task. 
+## conda activate labelme_env
+## python LabelMe_guide_Tamim.py
+
+## Call LabelME GUI Script
+labelme
+
