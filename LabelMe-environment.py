cf935ce694660b9976a34748ee03198a1e2c87e7
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2025-05-09 21:28:27 -0400

    Update LabelMe-environment.py

--- LabelMe-environment.py
+++ LabelMe-environment.py
@@ -1,13 +1,10 @@
+# ✅ First-time setup (run manually in Anaconda Prompt):
 conda create -n labelme_env python=3.9 -y
 conda activate labelme_env
-
 pip install pyqt5==5.15.9
 pip install labelme==5.2.1
 
-## use the following lines for LabelMe task. 
-## conda activate labelme_env
-## python LabelMe_guide_Tamim.py
-
-## Call LabelME GUI Script
-labelme
-
+# Follow next codes for each time when you start labelme for annoation
+# conda activate labelme_env
+# python LabelMe_guide_Tamim.py
+# labelme  # (optional GUI)
