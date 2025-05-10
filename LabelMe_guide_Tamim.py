4e57382b6e4f09437f1e33167f94e8075c1fbcc4
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2025-05-09 21:13:51 -0400

    Update LabelMe_guide_Tamim.py

--- LabelMe_guide_Tamim.py
+++ LabelMe_guide_Tamim.py
@@ -1,12 +1,3 @@
-## Use LabelMe via PyQt5 in a Clean Virtual Environment 
-conda create -n labelme_env python=3.9 -y
-conda activate labelme_env
-
-pip install pyqt5==5.15.9
-pip install labelme==5.2.1
-labelme
-
-
 ## Code for label, Task: Inclusion-Exclusion
 import glob
 from PIL import Image
